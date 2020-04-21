e18addbacf2c0133562c5befebc855e8f430a9af
ClementBreyne <63702906+ClementBreyne@users.noreply.github.com>
2020-04-21 08:34:59 +0200

    TP_Pizza2

---
+++ Utils/CustomModelState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TPModule5_2_BO;
+
+namespace TPModule5_2.Utils
+{
+    public class CustomModelState
+    {
+        public bool CheckListLength(IList LstToCheck )
+        {
+            if(LstToCheck.Count<5)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool CheckIdPate(int id)
+        {
+            if(id == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool CheckIngredient(List<Ingredient> LstToCheck1, List<Ingredient> LstToCheck2)
+        {
+            if(LstToCheck1.All(LstToCheck2.Contains)&& LstToCheck1.Count == LstToCheck2.Count)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
